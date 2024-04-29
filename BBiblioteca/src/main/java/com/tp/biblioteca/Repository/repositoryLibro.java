ea455d2e998746c72f1fa4eec8be6a2336488702
MrNarnia01 <brunonp03@gmail.com>
2024-04-29 16:16:30 -0300

    Pruebas Admin

--- BBiblioteca/src/main/java/com/tp/biblioteca/Repository/repositoryLibro.java
+++ BBiblioteca/src/main/java/com/tp/biblioteca/Repository/repositoryLibro.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface repositoryLibro extends JpaRepository<Libro, Long> {
+public interface    repositoryLibro extends JpaRepository<Libro, Long> {
     List<Libro> findByCantPag(int cantPag);
     List<Libro> findByTitulo(String titulo);
     List<Libro> findByAutor(String autor);
